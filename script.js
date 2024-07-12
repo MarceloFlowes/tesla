c5ee30e72e8b0ff33315131261bf4e047ca35578
MarceloFlowes <cecel.2008@gmail.com>
2024-07-12 17:03:23 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// alert("Seja bem vindo!") // uma alerta quando você entra no site
+// document = ir no HTML
+// querySelector = buscar o que eu quiser que ele busque
+
+let formulario = document.querySelector(".formulario")
+function aparecerFormulacio(){
+    formulario.style.left = "30%"
+
+}
+
+function desaparecerFormulario(){
+    formulario.style.left = "-50%"
+
+}
